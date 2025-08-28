144d32ad54a85b66e03b8697e23586386b0e04a7
Syed Uzair <47527596+uzaircs2022@users.noreply.github.com>
2025-08-28 15:55:33 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :handle_unauthorized
 
   private
 
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+  def handle_unauthorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = I18n.t("pundit.#{policy_name}.#{exception.query}",
+                           default: I18n.t("pundit.default", default: "You are not authorized to perform this action."))
     redirect_back(fallback_location: root_path)
   end
 end
