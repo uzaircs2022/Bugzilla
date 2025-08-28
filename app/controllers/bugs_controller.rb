09af536fd7c1b444a7af6f7e8700104f4029bede
Syed Uzair <47527596+uzaircs2022@users.noreply.github.com>
2025-08-28 15:57:24 +0500

    Update bugs_controller.rb

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,13 +1,13 @@
+# app/controllers/bugs_controller.rb
 class BugsController < ApplicationController
-  before_action :bug_project
+  before_action :set_project
+  before_action :set_bug, only: [:show, :edit, :update, :destroy, :bug_assignment, :status]
 
   def index
     @bugs = @project.bugs
   end
 
-  def show
-    @bug = @project.bugs.find(params[:id])
-  end
+  def show; end
 
   def new
     @bug = @project.bugs.new
@@ -17,63 +17,70 @@ class BugsController < ApplicationController
   def create
     @bug = @project.bugs.new(bug_params)
     @bug.reported_by = current_user
+    authorize @bug
 
     if @bug.save
-      redirect_to [@project, @bug], notice: 'Bug successfully Reported.'
+      redirect_to [@project, @bug], notice: "Bug successfully reported."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @bug = @project.bugs.find(params[:id])
     authorize @bug
   end
 
   def update
-    @bug = @project.bugs.find(params[:id])
+    authorize @bug
     if @bug.update(bug_params)
-      redirect_to [@project, @bug]
+      redirect_to [@project, @bug], notice: "Bug successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @bug = @project.bugs.find(params[:id])
+    authorize @bug
     @bug.destroy
-    redirect_to [@project, Bug] , notice: 'Bug successfully destroyed.'
+    redirect_to project_bugs_path(@project), notice: "Bug successfully destroyed."
   end
 
   def bug_assignment
-    if @project
-      @bug = @project.bugs.find(params[:id])
-      if @bug
-        if !(@bug.assigned_to_id)
-          @bug.assigned_to = current_user
-          @bug.save
-        end
-      end
+    authorize @bug, :assign?
+
+    if @bug.assigned_to_id.blank?
+      @bug.update(assigned_to: current_user)
+      flash[:notice] = "Bug assigned to you."
+    else
+      flash[:alert] = "Bug is already assigned."
     end
+
     redirect_to [@project, @bug]
   end
 
   def status
-    @bug = @project.bugs.find(params[:id])
-    if @project && @bug
-      @bug.status = params[:status]
-      @bug.save
+    authorize @bug, :update_status?
+
+    if @bug.update(status: params[:status])
+      flash[:notice] = "Bug status updated."
+    else
+      flash[:alert] = "Unable to update bug status."
     end
-    redirect_to  [@project, @bug]
+
+    redirect_to [@project, @bug]
   end
 
   private
 
-  def bug_project
+  def set_project
     @project = Project.find(params[:project_id])
   end
 
+  def set_bug
+    @bug = @project.bugs.find(params[:id])
+  end
+
   def bug_params
-    params.require(:bug).permit(:title, :description, :deadline, :bugtype, :status, :image )
+    params.require(:bug).permit(:title, :description, :deadline, :bugtype, :status, :image)
   end
 end
